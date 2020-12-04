beff91659182a3679d8f7b05c1025a4304892402
CarlaBruckmann <carlaabruckmann@gmail.com>
2020-12-04 15:32:29 -0300

    feat(app) added modal responsive
    Added modal responsive.

--- Netflix/src/main.js
+++ Netflix/src/main.js
@@ -13,55 +13,28 @@ $('.owl-carousel').owlCarousel({
             items: 5
         }
     }
-})
+});
 
-//details//
 
 $(document).ready(function() {
 
-    //seleciona os elementos a com atributo name="modal"
     $('a[name=modal]').click(function(e) {
-        //cancela o comportamento padrão do link
         e.preventDefault();
-
-        //armazena o atributo href do link
         var id = $(this).attr('href');
-
-        //armazena a largura e a altura da tela
         var maskHeight = $(document).height();
-        var maskWidth = $(window).width();
-
-        //Define largura e altura do div#mask iguais ás dimensões da tela
-        $('#mask').css({ 'width': maskWidth, 'height': maskHeight });
-
-        //efeito de transição
+        $('#mask').css({ 'height': maskHeight });
         $('#mask').fadeIn(1000);
         $('#mask').fadeTo("slow", 0.8);
-
-        //armazena a largura e a altura da janela
-        var winH = $(window).height();
-        var winW = $(window).width();
-        //centraliza na tela a janela popup
-        $(id).css('top', winH / 2 - $(id).height() / 2);
-        $(id).css('left', winW / 2 - $(id).width() / 2);
-        //efeito de transição
         $(id).fadeIn(2000);
     });
 
-    //se o botãoo fechar for clicado
     $('.window .close').click(function(e) {
-        //cancela o comportamento padrão do link
         e.preventDefault();
         $('#mask, .window').hide();
     });
 
-    //se div#mask for clicado
     $('#mask').click(function() {
         $(this).hide();
         $('.window').hide();
     });
-
-
-
-
 });
